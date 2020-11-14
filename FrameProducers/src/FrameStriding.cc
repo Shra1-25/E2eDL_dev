91c3d2c60ce0ddef9c605619feae5296a7e9ad0c
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-14 23:54:04 +0530

    Update FrameStriding.cc

--- FrameProducers/src/FrameStriding.cc
+++ FrameProducers/src/FrameStriding.cc
@@ -1,13 +1,13 @@
 #include "E2eDL/FrameProducers/interface/FrameStriding.h"
 
 // Striding input frames (vDetFrame by rowstrides and colstrides accordingly)
-e2e::Frame1D frameStriding(std::vector<float>& vDetFrame, int rows, int columns, int rowstrides, int colstrides){
-  e2e::Frame1D vStridedFrame ((rows*rowstrides)*(columns*colstrides),0);
+e2e::Frame1D frameStriding(e2e::Frame2D& vDetFrame, int rows, int columns, int rowstrides, int colstrides){
+  e2e::Frame2D vStridedFrame ((rows*rowstrides),((columns*colstrides),0));
   for (int rowidx=0; rowidx<rows; rowidx++){
     for (int colidx=0; colidx<columns; colidx++){
       for (int kernelrow=0; kernelrow<rowstrides; kernelrow++){
         for (int kernelcol=0; kernelcol<colstrides; kernelcol++){
-          vStridedFrame[(rowstrides*rowidx+kernelrow)*columns*colstrides+colstrides*colidx+kernelcol] = vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
+          vStridedFrame[(rowstrides*rowidx+kernelrow)][colstrides*colidx+kernelcol] = vDetFrame[rowidx][colidx]/(rowstrides*colstrides);
           //if(rowidx<5 && colidx<5) std::cout<<"("<<rowstrides*rowidx+kernelrow<<","<<colstrides*colidx+kernelcol<<"): "<<vStridedFrame[(rowstrides*rowidx+kernelrow)*columns*colstrides+colstrides*colidx+kernelcol]<<" "<<vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
         }
       }
