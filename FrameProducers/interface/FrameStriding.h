4dc3029d11a9224edcab2583d142ff37c9d1a7d1
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-15 00:21:32 +0530

    Update FrameStriding.h

--- FrameProducers/interface/FrameStriding.h
+++ FrameProducers/interface/FrameStriding.h
@@ -6,6 +6,6 @@
 using namespace std;
 #include "E2eDL/DataFormats/interface/FrameCollections.h"
 
-e2e::Frame1D frameStriding(e2e::Frame1D&, int, int, int, int);
+e2e::Frame2D frameStriding(e2e::Frame2D&, int, int, int, int);
 
 #endif
