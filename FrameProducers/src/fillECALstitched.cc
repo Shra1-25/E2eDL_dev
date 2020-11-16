e46346e29906ffaf111205045f3a82e01dea8c0e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-16 23:54:35 +0530

    Update fillECALstitched.cc

--- FrameProducers/src/fillECALstitched.cc
+++ FrameProducers/src/fillECALstitched.cc
@@ -1,4 +1,4 @@
-#include "E2eDL/FrameProducers/interface/DetFrameProducer.h"
+#include "E2eDL/FrameProducers/interface/fillECALstitched.h"
 
 // Fill stitched EEm_EB_EEp image /////////////////////............/
 // Store all ECAL event rechits into a stitched EEm_EB_EEp image 
