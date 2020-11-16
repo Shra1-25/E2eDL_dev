e5f42519a00672a28d0c629126c7db7ba4bc4776
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-16 22:53:58 +0530

    Create fillECALstitched.h

---
+++ FrameProducers/interface/fillECALstitched.h
@@ -0,0 +1,8 @@
+#ifndef RecoE2E_fillECALstitched_h
+#define RecoE2E_fillECALstitched_h
+#include "E2eDL/FrameProducers/interface/DetFrameProducer.h"
+
+namespace e2e {
+  void fillECALstitched   ( const edm::Event&, const edm::EventSetup& );
+}
+#endif
