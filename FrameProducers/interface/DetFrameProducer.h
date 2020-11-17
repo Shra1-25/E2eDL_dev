70fa7abb1bc37048627885e0db2f763ed00decc7
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 18:32:32 +0530

    Update DetFrameProducer.h

--- FrameProducers/interface/DetFrameProducer.h
+++ FrameProducers/interface/DetFrameProducer.h
@@ -108,10 +108,10 @@ class DetFrameProducer : public edm::stream::EDProducer<> {
 
       // ----------member data ---------------------------
       // Tokens
-      edm::EDGetTokenT<EcalRecHitCollection> EBRecHitCollectionT_; 
+      //edm::EDGetTokenT<EcalRecHitCollection> EBRecHitCollectionT_; 
       edm::EDGetTokenT<PhotonCollection> photonCollectionT_;
       edm::EDGetTokenT<HBHERecHitCollection> HBHERecHitCollectionT_;
-      edm::EDGetTokenT<EcalRecHitCollection> EERecHitCollectionT_;
+      //edm::EDGetTokenT<EcalRecHitCollection> EERecHitCollectionT_;
       edm::EDGetTokenT<reco::TrackCollection> trackCollectionT_;
       edm::EDGetTokenT<reco::VertexCollection> vertexCollectionT_;
       edm::EDGetTokenT<reco::PFJetCollection> jetCollectionT_;
