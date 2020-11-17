6ae9dd3aaaa570aabe37b576559ce06891bcc745
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 18:05:05 +0530

    Update fillECALstitched.h

--- FrameProducers/interface/fillECALstitched.h
+++ FrameProducers/interface/fillECALstitched.h
@@ -75,6 +75,6 @@
 #include "E2eDL/FrameProducers/interface/DetFrameProducer.h"
 
 namespace e2e {
-  e2e::Frame1D fillECALstitched   ( edm::Handle<EcalRecHitCollection> EBRecHitsH_, edm::Handle<EcalRecHitCollection> EERecHitsH_, edm::ESHandle<CaloGeometry> caloGeomH_ );
+  void e2e::fillECALstitched ( const edm::Event&, const edm::EventSetup&, e2e::Frame1D );
 }
 #endif
