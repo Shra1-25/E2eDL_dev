b247502a73450cab9d582913c7c6e84dd2c668f6
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 22:02:26 +0530

    Update DetFrameProducer.h

--- FrameProducers/interface/DetFrameProducer.h
+++ FrameProducers/interface/DetFrameProducer.h
@@ -204,7 +204,6 @@ static const int HBHE_IETA_MAX_HB = hcaldqm::constants::IETA_MAX_HB;//16;
 static const int HBHE_IETA_MIN_HB = hcaldqm::constants::IETA_MIN_HB;//1;
 static const int HBHE_IETA_MAX_HE = hcaldqm::constants::IETA_MAX_HE;//29;
 static const int HBHE_IETA_MAX_EB = hcaldqm::constants::IETA_MAX_HB + 1; // 17
-static const int HBHE_IPHI_NUM = hcaldqm::constants::IPHI_NUM;//72;
 static const int HBHE_IPHI_MIN = hcaldqm::constants::IPHI_MIN;//1;
 static const int HBHE_IPHI_MAX = hcaldqm::constants::IPHI_MAX;//72;
 static const int ECAL_IETA_MAX_EXT = 140;
