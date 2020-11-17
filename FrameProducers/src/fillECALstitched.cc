858280fabb60ce038dd3cef9f80d98cde56b4c2f
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 19:16:45 +0530

    Update fillECALstitched.cc

--- FrameProducers/src/fillECALstitched.cc
+++ FrameProducers/src/fillECALstitched.cc
@@ -45,7 +45,7 @@ void fillECAL_with_EEproj ( std::vector<float>& vECAL_energy_, TH2F *hEvt_EE_ene
 } // fillECAL_with_EEproj
 
 // Fill stitched EE-, EB, EE+ rechits ________________________________________________________//
-void e2e::fillECALstitched ( const edm::Event& iEvent, const edm::EventSetup& iSetup, e2e::Frame1D vECAL_energy_ ) {
+void e2e::fillECALstitched ( const edm::Event& iEvent, const edm::EventSetup& iSetup, edm::EDGetTokenT<EcalRecHitCollection>, edm::EDGetTokenT<EcalRecHitCollection>, e2e::Frame1D vECAL_energy_ ) {
   
   // Intermediate helper histogram (single event only)
   hEvt_EE_energy[0] = new TH2F("evt_EEm_energy", "E(i#phi,i#eta);i#phi;i#eta",
@@ -63,11 +63,6 @@ void e2e::fillECALstitched ( const edm::Event& iEvent, const edm::EventSetup& iS
 
   vECAL_energy_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
   for ( int iz(0); iz < nEE; ++iz ) hEvt_EE_energy[iz]->Reset();
-  
-  edm::EDGetTokenT<EcalRecHitCollection> EBRecHitCollectionT_; 
-  edm::EDGetTokenT<EcalRecHitCollection> EERecHitCollectionT_;
-  EBRecHitCollectionT_    = consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("reducedEBRecHitCollection"));
-  EERecHitCollectionT_    = consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("reducedEERecHitCollection"));
   
   edm::Handle<EcalRecHitCollection> EBRecHitsH_;
   iEvent.getByToken( EBRecHitCollectionT_, EBRecHitsH_ );
