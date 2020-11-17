b1879adb7948c16c3945e12caa84e534713430c1
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 23:18:10 +0530

    Update constants.h

--- FrameProducers/interface/constants.h
+++ FrameProducers/interface/constants.h
@@ -10,7 +10,6 @@ static const unsigned int nDetFrameW = 360;
 static const bool debug = false;
 
 static const std::string adj_projections[Nadjproj] = {"_5x5", "_3x3"};
-static const int HBHE_IPHI_NUM = hcaldqm::constants::IPHI_NUM;//72;
 unsigned int granularityMultiPhi[Nadjproj];
 unsigned int granularityMultiEta[Nadjproj];
       
