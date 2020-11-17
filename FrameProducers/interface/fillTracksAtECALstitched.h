5227efbbaaccbc7b05732526d3156cdd6b9020ae
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-17 20:27:30 +0530

    Update fillTracksAtECALstitched.h

--- FrameProducers/interface/fillTracksAtECALstitched.h
+++ FrameProducers/interface/fillTracksAtECALstitched.h
@@ -76,5 +76,7 @@
 
 namespace e2e {
   void fillTracksAtECALstitched ( const edm::Event&, const edm::EventSetup&, unsigned int, edm::EDGetTokenT<EcalRecHitCollection>, edm::EDGetTokenT<EcalRecHitCollection>, e2e::Frame2D, e2e::Frame2D, e2e::Frame2D );
+  std::vector<int> findSubcrystal(const CaloGeometry* caloGeom, const float& eta, const float& phi, const int& granularityMultiEta, const int& granularityMultiPhi);
+  void fillByBinNumber(TH2F * histo, const std::vector<int>& phi_eta, const float& value);
 }
 #endif
