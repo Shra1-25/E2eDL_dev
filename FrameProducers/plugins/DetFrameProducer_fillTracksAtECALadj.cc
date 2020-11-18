26405f6346734a6d96634f4678d58cec5dd23cc7
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-11-18 17:50:37 +0530

    Update DetFrameProducer_fillTracksAtECALadj.cc

--- FrameProducers/plugins/DetFrameProducer_fillTracksAtECALadj.cc
+++ FrameProducers/plugins/DetFrameProducer_fillTracksAtECALadj.cc
@@ -1,5 +1,5 @@
 #include "E2eDL/FrameProducers/interface/DetFrameProducer.h"
-
+/*
 TH2F *hEvt_Adj_tracks[Nadjproj];
 TH2F *hEvt_Adj_tracksPt[Nadjproj];
 TH2F *hEvt_Adj_tracksPt_max[Nadjproj];
@@ -264,4 +264,4 @@ void DetFrameProducer::fillTracksAtECALadjustable ( const edm::Event& iEvent, co
       vECALadj_tracksPt_max_[proj][index1d]=hEvt_Adj_tracksPt_max[proj]->GetBinContent(iphi,ieta);
     }
   }
-} // fillTracksAtECALadjustable()
+} // fillTracksAtECALadjustable()*/
